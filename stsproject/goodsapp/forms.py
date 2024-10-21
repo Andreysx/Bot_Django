e31cd7bae3c520b457625a3ed2d900549f8d16d3
Andreye <drontop1999@mail>
2024-10-21 21:48:22 +0300

    Upgrade upload template and form

--- stsproject/goodsapp/forms.py
+++ stsproject/goodsapp/forms.py
@@ -3,5 +3,7 @@ from django import forms
 
 class ExcelUploadForm(forms.Form):
     file = forms.FileField(label='Выберите файл')
-    name = forms.CharField(max_length=50, label='Название заказа', required=True)
-    description = forms.CharField(widget=forms.Textarea, label='Описание заказа', required=True)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Название заказа'}),
+                           max_length=50, label='Название заказа', required=True)
+    description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Описание'}),
+                                  label='Описание заказа', required=True)
