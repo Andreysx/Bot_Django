eb8a69728c372108628d129689cc31f71211cf00
Andreye <drontop1999@mail>
2024-10-24 23:55:55 +0300

    Change telegramm bot

--- stsproject/telegram_bot.py
+++ stsproject/telegram_bot.py
@@ -25,22 +25,36 @@ user_data = {}
 # Функция для обработки команды /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет Выбери команду:\n/list_orders - список заказов\n/choose_order - выбрать заказ')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_list_orders = types.KeyboardButton("📋 Список заказов")
+    button_choose_order = types.KeyboardButton("🛒 Выбрать заказ")
+
+    markup.add(button_list_orders, button_choose_order)
+
+    bot.send_message(message.chat.id, 'Привет! Выберите команду:', reply_markup=markup)
+
 
 # Функция для вывода всех заказов
-@bot.message_handler(commands=['list_orders'])
+@bot.message_handler(func=lambda message: message.text == "📋 Список заказов")
 def list_orders_message(message):
     orders = Orders.objects.all()
     message_text = 'Список заказов:\n'
-    for order in orders:
-        message_text += f"{order.name} - {order.date}\n"
+
+    if orders.exists():
+        for order in orders:
+            message_text += f"{order.name} - {order.date}\n"
+    else:
+        message_text = "Нет доступных заказов."
+
     bot.send_message(message.chat.id, message_text)
 
-# Функция для выбора заказа по имени
-@bot.message_handler(commands=['choose_order'])
+
+# Функция для обработки выбора конкретного заказа
+@bot.message_handler(func=lambda message: message.text == "🛒 Выбрать заказ")
 def choose_order_message(message):
     bot.send_message(message.chat.id, "Введите имя заказа для выбора:")
     user_data[message.chat.id] = {'waiting_for_order': True}
+
 
 # Функция для обработки сообщения с именем заказа
 @bot.message_handler(content_types=['text'], func=lambda message: message.text and not message.text.startswith('/'))
@@ -51,7 +65,7 @@ def handle_order_name(message):
         if orders.exists():
             order = orders.first()  # Получаем первый найденный заказ
             user_data[message.chat.id] = {'chosen_order_id': order.id}
-            bot.send_message(message.chat.id, f"Выбран заказ: {order.name} - {order.date}")
+            bot.send_message(message.chat.id, f"Выбран заказ: {order.name}")
             bot.send_message(message.chat.id, "Введите артикул продукта для поиска:")
         else:
             bot.send_message(message.chat.id, "Заказ не найден. Пожалуйста, попробуйте снова.")
@@ -67,22 +81,19 @@ def handle_order_name(message):
             products = Products.objects.filter(order=order, article=article)
             if products.exists():
                 product = products.first()
-                bot.send_message(message.chat.id, f"Продукт найден:\nАртикул:  {product.article} "
-                                                  f"\nНазвание:  {product.name}" 
-                                                  f"\nКоличество:  {product.quantity}"
-                                                  f"\nЦена за единицу:  {product.unit_price}"
-                                                    f"\nВес:  {product.weight}"
-                                                    f"\nСтоимость:  {product.amount}")
-
+                bot.send_message(message.chat.id,
+                                 f"Продукт найден:\nАртикул: {product.article}\nНазвание: {product.name}\nКоличество: {product.quantity} штук\nЦена за единицу: {product.unit_price} RUB\nВес: {product.weight} кг\nСтоимость: {product.amount} RUB")
             else:
                 bot.send_message(message.chat.id, "Продукт не найден.")
         else:
             bot.send_message(message.chat.id, "Сначала выберите заказ.")
 
+
 # Функция для обработки инлайн кнопок (не используется в этом случае)
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
     pass  # Not needed in this scenario
+
 
 # Запуск бота
 if __name__ == '__main__':
